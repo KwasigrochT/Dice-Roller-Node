717b056aec6c70faf2e69072b3dcad44d912cce4
KwasigrochT <158532345+KwasigrochT@users.noreply.github.com>
2025-03-02 11:34:12 -0600

    fix cors issue

--- server.js
+++ server.js
@@ -5,7 +5,14 @@ const app = express()
 const PORT  = process.env.PORT || 3000
 
 
-app.use(cors())
+const corsOptions = {
+    origin: "https://green-bush-0a3493e10.6.azurestaticapps.net",
+    methods: "GET",
+    allowedHeaders: ["Content-Type"]
+}
+
+
+app.use(cors(corsOptions))
 
 app.use(express.json())
 
@@ -31,7 +38,7 @@ app.get ("/wake-up", (req, res) => {
 })
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "https://green-bush-0a3493e10.6.azurestaticapps.net")
     res.header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
     res.header("Access-Control-Allow-Headers", "Content-Type")
     next();
