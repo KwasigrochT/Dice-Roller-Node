284dbc1a215e14d5e4966ab8372279348f4aef6d
KwasigrochT <158532345+KwasigrochT@users.noreply.github.com>
2025-03-02 14:19:31 -0600

    added index

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const cors = require("cors")
+const path = require("path")
 
 app = express()
 const PORT  = process.env.PORT || 3000
 
 app.use(cors({origin: '*'}))
 app.use(express.json())
+
+
+app.use(express.static(path.join(__dirname, "public")))
+
 
 function rollDie() {
     return Math.floor(Math.random() * 6) + 1;
@@ -27,6 +32,10 @@ app.get("/roll-dice", (req, res) => {
 app.get ("/wake-up", (req, res) => {
     res.json({ message: "Server is awake"})
 })
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
